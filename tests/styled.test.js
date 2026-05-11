b368033aa05f14d7f5ce565d7ca07cef187eb504
Brian Teague <br.teague@gmail.com>
2026-05-11 15:06:40 -0400

    Build cache on initial render for SSR

--- tests/styled.test.js
+++ tests/styled.test.js
@@ -3,28 +3,28 @@ import { styled } from '../lib/styled.js';
 import { renderToString } from 'hyperapp-render';
 import { extractCss } from '../lib/css.js';
 
-test('styled.button renders server-side HTML', t => {
-  const Button = styled.button`
-    background: blue;
-  `;
+test('styled.button renders server-side HTML', (t) => {
+   const Button = styled.button`
+      background: blue;
+   `;
 
-  const vnode = Button({}, ['Click']);
-  const html = renderToString(vnode);
-  const style = extractCss();
+   const vnode = Button({}, ['Click']);
+   const html = renderToString(vnode);
+   const style = extractCss();
 
-  t.is(html, '<button class="hyper-1">Click</button>');
-  t.is(style, '.hyper-1{background:blue;}');
+   t.is(html, '<button class="hyper-1">Click</button>');
+   t.is(style, '.hyper-1{background:blue;}');
 });
 
-test('styled.button merges existing class prop', t => {
-  const Button = styled.button`
-    background: green;
-  `;
+test('styled.button merges existing class prop', (t) => {
+   const Button = styled.button`
+      background: green;
+   `;
 
-  const vnode = Button({ class: 'external' }, ['Submit']);
-  const html = renderToString(vnode);
-  const style = extractCss();
+   const vnode = Button({ class: 'external' }, ['Submit']);
+   const html = renderToString(vnode);
+   const style = extractCss();
 
-  t.is(html, '<button class="hyper-2 external">Submit</button>');
-  t.true(style.includes('.hyper-2{background:green;}'));
+   t.is(html, '<button class="hyper-2 external">Submit</button>');
+   t.true(style.includes('.hyper-2{background:green;}'));
 });
